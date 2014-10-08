9b4e0b75e0acf6f0579d101fa379107ed5381c4f
Jonas Krat <jok@peytz.dk>
2014-10-08 10:08:55 +0200

    Made to suite my needs
    I did some organization within the template and scss folders to suite
    my way of styling and keeping the look more simple when looking for
    templates. Did a optimization of the icons - that I am not going to use
    any way.

--- template.php
+++ template.php
@@ -1,5 +1,14 @@
 <?php
-
-	// Add functions here
+	/**
+	* Removes non essential stylesheets with hook_css_alter().
+	*/
+	function bb_css_alter(&$css) {
+	  foreach ($css as $key => $value) {
+	    if ($value['group'] != CSS_THEME) {
+	      $exclude[$key] = FALSE;
+	    }
+  	}
+  	$css = array_diff_key($css, $exclude);
+	}
 
 ?>
