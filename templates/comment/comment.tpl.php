f03687de1c46e29f1597107f4f6b1c80c745a523
Jonas Krat <jok@peytz.dk>
2014-10-20 08:41:22 +0200

    Tabs to double space
    A backender came to me and complained about the tabs are not allowed in
    Drupal coding standard. I removed thoose in the templates. What are
    they doing in my templates files anyway?

--- templates/comment/comment.tpl.php
+++ templates/comment/comment.tpl.php
@@ -1,33 +1,33 @@
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-	
-	<?php print $picture ?>
 
-	<?php if ($new): ?>
-		<span class="new"><?php print $new ?></span>
-	<?php endif; ?>
+  <?php print $picture ?>
 
-	<?php print render($title_prefix); ?>
-		<h3<?php print $title_attributes; ?>><?php print $title ?></h3>
-	<?php print render($title_suffix); ?>
+  <?php if ($new): ?>
+    <span class="new"><?php print $new ?></span>
+  <?php endif; ?>
 
-	<div class="submitted">
-		<?php print $permalink; ?>
-		<?php print $submitted; ?>
-	</div>
+  <?php print render($title_prefix); ?>
+    <h3<?php print $title_attributes; ?>><?php print $title ?></h3>
+  <?php print render($title_suffix); ?>
 
-	<div class="content"<?php print $content_attributes; ?>>
-		<?php
-		// We hide the comments and links now so that we can render them later.
-		hide($content['links']);
-		print render($content);
-		?>
-		<?php if ($signature): ?>
-			<div class="user-signature clearfix">
-				<?php print $signature ?>
-			</div>
-		<?php endif; ?>
-	</div>
+  <div class="submitted">
+    <?php print $permalink; ?>
+    <?php print $submitted; ?>
+  </div>
 
-	<?php print render($content['links']) ?>
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['links']);
+    print render($content);
+    ?>
+    <?php if ($signature): ?>
+      <div class="user-signature clearfix">
+        <?php print $signature ?>
+      </div>
+    <?php endif; ?>
+  </div>
+
+  <?php print render($content['links']) ?>
 
 </div>
--- templates/system/region.tpl.php
+++ templates/system/region.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($content): ?>
-	<div class="<?php print $classes; ?>">
-		<?php print $content; ?>
-	</div>
+  <div class="<?php print $classes; ?>">
+    <?php print $content; ?>
+  </div>
 <?php endif; ?>
